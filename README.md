fee5573a78dc905732ec19668696206dba98ff49
Thulio Philipe <thulioph@gmail.com>
2018-02-15 17:11:20 -0300

    Adds the initial scripts.

---
+++ README.md
@@ -0,0 +1,136 @@
+# Macbook Setup
+
+## Instruções
+
+- Execute o arquivo `env.sh` para setar algumas configurações no Mac
+- Execute o arquivo `install.sh` para iniciar a instalação das aplicações
+  - Homebrew
+    - https://brew.sh/index_pt-br.html
+  - Homebrew Cask
+    - https://caskroom.github.io/
+    - https://github.com/sindresorhus/quick-look-plugins
+  - Franz
+  - Google Chrome
+  - Firefox
+  - Slack
+  - Flux
+  - GPG Keychain
+    - https://gpgtools.org/
+    - https://help.github.com/articles/generating-a-new-gpg-key/
+  - Iterm2
+  - Oh My ZSH
+    - https://github.com/caiogondim/bullet-train.zsh
+    - https://github.com/zsh-users/zsh-autosuggestions
+  - Evernote
+  - Spotify
+  - Postgress
+  - Docker
+  - Photoshop
+  - Sketch
+  - Zeplin
+  - Sublime Text
+  - Visual Studio Code
+    - https://www.smashingmagazine.com/2018/01/visual-studio-code/
+  - Harvest
+  - Be Focused (Pomodoro APP)
+  - Rescue Time
+  - Monosnap
+  - Theine (prevent mac to sleep)
+  - Android Studio
+  - Node Environment
+    - https://github.com/tj/n
+    - https://github.com/creationix/nvm
+    - https://medium.com/gitignore/nvm-vs-n-f34ebca314ea
+  - Ngrok
+    - https://ngrok.com/
+  - AppCleaner
+    - https://freemacsoft.net/appcleaner/
+
+# Mais informações
+
+- https://github.com/kelvinst/dotfiles
+- https://gist.github.com/kelvinst/331aff32508e2517afbd
+- https://gist.github.com/pksunkara/988716
+- https://github.com/djalmaaraujo/macbook-bootstrap
+
+----
+
+## Pre
+- Backup .ssh
+- Backup /etc/hosts
+- Backup Windows VM's
+- Backup ~/dev
+- Backup ~/prtrello.json
+- Backup MySQL + PGSQL Databases (usually I don't care)
+- Backup ~/Downloads
+- Backup ~/Desktop (should stop using.. but)
+
+## Installations
+
+### Essencial
+- Mac OS Newest version installed? Check for update
+- Xcode (App Store)
+  - Install Command line tools (Includes GIT)
+  ```bash
+  xcode-select --install
+  ```
+
+- Change Terminal Shell to **ZSH**
+- Install Dropbox [[Download](http://dropbox.com/)]
+- Install Homebrew [[Download](http://mxcl.github.io/homebrew/)]
+- Install Node [[Download](http://nodejs.org/)]
+- Install https://github.com/tj/n
+- Create ~/dev folder
+- Install dotfiles from Bitbucket
+- Create symlink from .oh-my-z-shell to dev/me/dotfiles
+	```bash
+	ln -s "/Users/myuser/dev/me/dotfiles/plugins/djalma/djalma.plugin.zsh" /Users/myuser/.oh-my-zsh/plugins/djalma/djalma.plugin.zsh
+	```
+- Install Z
+```bash
+# Save this in ~/z.sh
+# Paste . ~/z.sh in .zshrc
+
+https://raw.githubusercontent.com/rupa/z/master/z.sh
+```
+
+- Install 1Password [[Download](https://agilebits.com/onepassword)]
+	- Use archive file from Dropbox
+- Install iTerm.app [[Download](http://www.iterm2.com/#/section/home)]
+- Install Breeze App [[Download](http://www.autumnapps.com/breeze/)]
+- Organize my Finder Favorites like the preview [Preview](http://cl.ly/image/2V3s3I0i3O2n)
+
+### System Config
+```bash
+curl -L https://github.com/djalmaaraujo/macbook-bootstrap/raw/master/run-me.sh | sh
+```
+
+### Personal
+- Install Spotify [[Download](http://spotify.com/)]
+- Fantastical
+- Dollar Value
+- Caffeine
+- NordVPN
+- Reacue Time
+- Display Maid
+
+### Work
+- ~~Install VS Code [Download](https://code.visualstudio.com/)~~ - Using Sublime Again 
+- Install Bartender 3  [[Download](https://www.macbartender.com)]
+- ~~Install Beamer App [[Download](http://beamer-app.com/download?cid=275562564.1437688109)]~~
+- Sketch
+- TunnelBlick
+- Photoshop
+- Android Studio
+
+### Not required
+- ~~ScreenHero~~
+- ~~iStats menus5~~
+- Install VirtualBox [[Download](https://www.virtualbox.org/)] (Vm's Backup HD)
+- Install Smartgit [[Download](http://www.syntevo.com/smartgithg/)]
+- Chrome launcher app
+- Postgres App
+
+### Ruby/Node Environment
+- Install RVM  [[Download](https://rvm.io/)]
+- Install N  [[Download](https://github.com/tj/n)]
---
+++ env.sh
@@ -0,0 +1,137 @@
+printf "################ Setting up the Environment ################\n\n"
+
+printf "Show hidden files\n"
+defaults write com.apple.finder AppleShowAllFiles YES
+
+printf "Enable key repeat\n"
+defaults write -g ApplePressAndHoldEnabled -bool false
+
+printf "Remove all stupid Dock application\n"
+defaults write com.apple.dock static-only -bool TRUE; killall Dock
+
+printf "\n\nDock\n\n"
+
+printf "Set the icon size of Dock items to 48 pixels\n"
+defaults write com.apple.dock tilesize -int 48
+
+printf "Dock: enable magnification\n"
+defaults write com.apple.dock magnification -bool false
+
+printf "Set magnification icon size to 80 pixels\n"
+defaults write com.apple.dock largesize -float 80
+
+printf "Dock: minimize windows into their applications icon\n"
+defaults write com.apple.dock minimize-to-application -bool true
+
+printf "Dock: enable spring loading for all Dock items\n"
+defaults write com.apple.dock enable-spring-load-actions-on-all-items -bool true
+
+printf "Dock: enable animation when opening applications\n"
+defaults write com.apple.dock launchanim -bool false
+
+printf "Speed up Mission Control animations\n"
+defaults write com.apple.dock expose-animation-duration -float 0.1
+
+printf "General: enable Dashboard as an overlay\n"
+defaults write com.apple.dock dashboard-in-overlay -bool false
+
+printf "General: automatically rearrange Spaces based on most recent use\n"
+defaults write com.apple.dock mru-spaces -bool true
+
+printf "Dock: make icons of hidden applications translucent\n"
+defaults write com.apple.dock showhidden -bool false
+
+printf "\n\nScreen\n\n"
+
+printf "Require password immediately after sleep or screen saver begins\n"
+defaults write com.apple.screensaver askForPassword -int 1
+defaults write com.apple.screensaver askForPasswordDelay -int 0
+
+printf "Save screenshots in PNG format (other options: BMP, GIF, JPG, PDF, TIFF)\n"
+defaults write com.apple.screencapture type -string "png"
+
+printf "Save screenshots in Documents folder\n"
+defaults write com.apple.screencapture location /Users/thulioph/Documents/
+
+printf "Enable subpixel font rendering on non-Apple LCDs\n"
+defaults write NSGlobalDomain AppleFontSmoothing -int 2
+
+printf "\n\nFinder\n\n"
+
+printf "Finder: allow quitting via ⌘ + Q; doing so will also hide desktop icons\n"
+defaults write com.apple.finder QuitMenuItem -bool true
+
+printf "Finder: disable window and Get Info animations\n"
+defaults write com.apple.finder DisableAllAnimations -bool true
+
+printf "General: Enable icons for hard drives, servers, and removable media on the desktop\n"
+defaults write com.apple.finder ShowExternalHardDrivesOnDesktop -bool true
+defaults write com.apple.finder ShowHardDrivesOnDesktop -bool true
+defaults write com.apple.finder ShowMountedServersOnDesktop -bool true
+defaults write com.apple.finder ShowRemovableMediaOnDesktop -bool true
+
+printf "Finder: show hidden files by default\n"
+defaults write com.apple.finder AppleShowAllFiles -bool true
+
+printf "Finder: show all filename extensions\n"
+defaults write NSGlobalDomain AppleShowAllExtensions -bool true
+
+printf "Finder: show status bar\n"
+defaults write com.apple.finder ShowStatusBar -bool true
+
+printf "Finder: show path bar\n"
+defaults write com.apple.finder ShowPathbar -bool true
+
+printf "Finder: allow text selection in Quick Look\n"
+defaults write com.apple.finder QLEnableTextSelection -bool true
+
+printf "Finder: display full path as Finder window title\n"
+defaults write com.apple.finder _FXShowPosixPathInTitle -bool true
+
+printf "When performing a search, search the current folder by default\n"
+defaults write com.apple.finder FXDefaultSearchScope -string "SCcf"
+
+printf "General: enable the warning when changing a file extension\n"
+defaults write com.apple.finder FXEnableExtensionChangeWarning -bool true
+
+printf "Finder: enable spring loading for directories\n"
+defaults write NSGlobalDomain com.apple.springing.enabled -bool false
+
+printf "Remove the spring loading delay for directories\n"
+defaults write NSGlobalDomain com.apple.springing.delay -float 0
+
+printf "General: create .DS_Store files on network volumes\n"
+defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool true
+defaults write com.apple.desktopservices DSDontWriteUSBStores -bool true
+
+printf "Finder: automatically open a new window when a volume is mounted\n"
+defaults write com.apple.frameworks.diskimages auto-open-ro-root -bool true
+defaults write com.apple.frameworks.diskimages auto-open-rw-root -bool true
+defaults write com.apple.finder OpenWindowForNewRemovableDisk -bool true
+
+printf "Use columns view in all Finder windows by default\n"
+printf "Four-letter codes for the other view modes: icnv, Nlsv, Flwv\n"
+defaults write com.apple.finder FXPreferredViewStyle -string "clmv"
+
+printf "General: enable the warning before emptying the Trash\n"
+defaults write com.apple.finder WarnOnEmptyTrash -bool true
+
+# printf "Finder: enable AirDrop over Ethernet and on unsupported Macs running Lion\n"
+# defaults write com.apple.NetworkBrowser BrowseAllInterfaces -bool true
+
+printf "Show the ~/Library folder\n"
+chflags nohidden ~/Library
+
+printf "Allow Apps from Anywhere in Gatekeeper for macOS Sierra\n"
+sudo spctl --master-disable
+
+printf "Use Plain Text Mode as Default\n"
+defaults write com.apple.TextEdit RichText -int 0
+
+printf "Expand Save Panel by Default\n"
+defaults write -g NSNavPanelExpandedStateForSaveMode -bool true && defaults write -g NSNavPanelExpandedStateForSaveMode2 -bool true
+
+printf "Full Keyboard Access - All Controls\n"
+defaults write NSGlobalDomain AppleKeyboardUIMode -int 3
+
+printf "################ 💩 DONE 💩 ################\n\n"
